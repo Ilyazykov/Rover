e424aa14fab4e19f87538b343a89cb13ac99f3b7
ilyazykov <ilya.zykov@icloud.com>
2017-02-22 02:00:58 +0300

    tests are passed

--- rover/src/main/java/netcracker/intensive/rover/programmable/ProgrammableRover.java
+++ rover/src/main/java/netcracker/intensive/rover/programmable/ProgrammableRover.java
@@ -2,14 +2,23 @@ package netcracker.intensive.rover.programmable;
 
 import netcracker.intensive.rover.GroundVisor;
 import netcracker.intensive.rover.Rover;
+import netcracker.intensive.rover.command.RoverCommand;
 import netcracker.intensive.rover.stats.SimpleRoverStatsModule;
+
+import javax.management.openmbean.CompositeData;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Этот класс должен уметь все то, что умеет обычный Rover, но при этом он еще должен уметь выполнять программы,
  * содержащиеся в файлах
  */
 public class ProgrammableRover extends Rover {
-    SimpleRoverStatsModule simpleRoverStatsModule;
+    private SimpleRoverStatsModule simpleRoverStatsModule;
+    private RoverCommandParser commandParser;
+    private Map<String, Object> settings;
 
     public ProgrammableRover(GroundVisor groundVisor, SimpleRoverStatsModule simpleRoverStatsModule) {
         super(groundVisor);
@@ -17,6 +26,17 @@ public class ProgrammableRover extends Rover {
     }
 
     public void executeProgramFile(String file) {
-        System.out.println(file);
+        commandParser = new RoverCommandParser(this, file);
+        RoverProgram program = commandParser.getProgram();
+        Collection<RoverCommand> commands = program.getCommands();
+        settings = program.getSettings();
+
+        for (RoverCommand command : commands) {
+            command.execute();
+        }
+    }
+
+    public Map<String,Object> getSettings() {
+        return Collections.unmodifiableMap(settings);
     }
 }
--- rover/src/test/java/netcracker/intensive/rover/programmable/ProgrammableRoverTest.java
+++ rover/src/test/java/netcracker/intensive/rover/programmable/ProgrammableRoverTest.java
@@ -21,39 +21,39 @@ public class ProgrammableRoverTest extends AbstractRoverTest {
         testedInstance = new ProgrammableRover(new GroundVisor(GROUND_3x3), new SimpleRoverStatsModule());
     }
 
-//    @Test
-//    public void testExecuteProgramFile() {
-//        String file = "program.txt";
-//
-//        testedInstance.executeProgramFile(file);
-//
-//        assertEquals(new Point(1, 0), testedInstance.getCurrentPosition());
-//    }
+    @Test
+    public void testExecuteProgramFile() {
+        String file = "program.txt";
 
-//    @Test
-//    public void testExecuteProgramFileFillsStatsSettings() {
-//        String file = "program_with_stats.txt";
-//
-//        testedInstance.executeProgramFile(file);
-//
-//        assertEquals(true, testedInstance.getSettings().get(RoverProgram.STATS));
-//    }
-//
-//    @Test
-//    public void testExecuteProgramFileFillsLoggingSettings() {
-//        String file = "program_with_log.txt";
-//
-//        testedInstance.executeProgramFile(file);
-//
-//        assertEquals(true, testedInstance.getSettings().get(RoverProgram.LOG));
-//    }
-//
-//    @Test(expected = UnsupportedOperationException.class)
-//    public void testGetSettingsReturnsImmutableObject() {
-//        String file = "program_with_log.txt";
-//
-//        testedInstance.executeProgramFile(file);
-//
-//        testedInstance.getSettings().put("test", "value");
-//    }
+        testedInstance.executeProgramFile(file);
+
+        assertEquals(new Point(1, 0), testedInstance.getCurrentPosition());
+    }
+
+    @Test
+    public void testExecuteProgramFileFillsStatsSettings() {
+        String file = "program_with_stats.txt";
+
+        testedInstance.executeProgramFile(file);
+
+        assertEquals(true, testedInstance.getSettings().get(RoverProgram.STATS));
+    }
+
+    @Test
+    public void testExecuteProgramFileFillsLoggingSettings() {
+        String file = "program_with_log.txt";
+
+        testedInstance.executeProgramFile(file);
+
+        assertEquals(true, testedInstance.getSettings().get(RoverProgram.LOG));
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void testGetSettingsReturnsImmutableObject() {
+        String file = "program_with_log.txt";
+
+        testedInstance.executeProgramFile(file);
+
+        testedInstance.getSettings().put("test", "value");
+    }
 }
